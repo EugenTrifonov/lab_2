dce11ba32d1a29dd85eebd067c91a21ac8eb7eb9
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-14 23:42:30 +0300

    Update train_transfer.py

--- train_transfer.py
+++ train_transfer.py
@@ -15,6 +15,7 @@ from tensorflow.keras.applications import EfficientNetB0
 from tensorflow.python import keras as keras
 from tensorflow.python.keras.callbacks import LearningRateScheduler
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers.experimental import preprocessing
 
 # Avoid greedy memory allocation to allow shared GPU usage
 gpus = tf.config.experimental.list_physical_devices('GPU')
