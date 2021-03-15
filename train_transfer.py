a96b2c99453b478200bbd7c888a206ed27bc5534
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-15 12:50:55 +0300

    Update train_transfer.py

--- train_transfer.py
+++ train_transfer.py
@@ -61,7 +61,7 @@ def create_dataset(filenames, batch_size):
 
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
-  model = EfficientNetB0(include_top=False,input_tensor=inputs,weights="imagenet")
+  model = EfficientNetB0(input_tensor=inputs,weights="imagenet")
   model.trainable=False
   x = tf.keras.layers.GlobalAveragePooling2D()(model.output)
   outputs = tf.keras.layers.Dense(NUM_CLASSES, activation=tf.keras.activations.softmax)(x)
