21a40607afc83902c92938b33b7a24bf0dcf2fa7
EugenTrifonov <80068414+EugenTrifonov@users.noreply.github.com>
2021-03-15 14:23:41 +0300

    Update train_transfer.py

--- train_transfer.py
+++ train_transfer.py
@@ -67,7 +67,7 @@ def create_dataset(filenames, batch_size):
 def build_model():
   inputs = tf.keras.Input(shape=(RESIZE_TO, RESIZE_TO, 3))
   x = data_augmentation(inputs)
-  #x= rescale(x)
+  x= rescale(x)
   x = preprocess_input(x)
   x = EfficientNetB0(include_top=False,input_tensor=x,weights="imagenet")
   x.trainable=False
